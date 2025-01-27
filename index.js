25fb6de98d46b5ddfcee4e53492fc295a094e000
Jubin <jubinphilip25@gmail.com>
2025-01-27 23:36:18 +0530

    commit

--- index.js
+++ index.js
@@ -4,13 +4,29 @@ import dotenv from 'dotenv'
 dotenv.config()
 import router from './routes/routes.js'
 
-const app=express()
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+const allowedOrigins = [
+    'https://restaurant-client-git-main-jubinphilips-projects.vercel.app/',
+    'https://restaurant-client-mnmfmjw6q-jubinphilips-projects.vercel.app/',
+    'http://localhost:3000'
+];
 
-app.use('/user',router)
-app.listen(process.env.PORT,()=>
-{
-    console.log("server running")
-})
+const app = express();
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/user', router);
+
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port", process.env.PORT);
+});
